fb4806b2c4dd1bebf3e117849732ffb8540924db
ナモすけ <dynamorollerbot@gmail.com>
2024-12-08 15:18:42 +0900

    概要を書く

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "「年末」を「調整」してくれるアプリ" },
+    {
+      name: "description",
+      content:
+        "このアプリでは「年末」を「調整」できます。※年末調整ではありません。",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main>
+      <header className="bg-sky-200 h-36">
+        <h1>「年末」を「調整」してくれるアプリ</h1>
+      </header>
+      <div className="w-fit mx-auto p-5 leading-7">
+        <p>
+          このアプリでは「年末」を「調整」できます。
+          <br />
+          ※年末調整ではありません。
+        </p>
+        <p>好きな日をクリックして年末にしてみよう！</p>
+      </div>
+    </main>
+  );
 }
